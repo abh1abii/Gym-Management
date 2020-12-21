2ab90c0985da6a19b5773b410ab1461f810c683f
Abhishek <Abii@AbiiMBP.local>
2020-12-21 23:07:46 +0530

    Backend on sql and adminPanel.php

--- GymAdmin.sql
+++ GymAdmin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2020 at 08:23 AM
+-- Generation Time: Dec 21, 2020 at 05:09 PM
 -- Server version: 8.0.22
 -- PHP Version: 7.3.22-(to be removed in future macOS)
 
@@ -43,6 +43,29 @@ INSERT INTO `admin_login` (`admin_name`, `admin_password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Members`
+--
+
+CREATE TABLE `Members` (
+  `First_name` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `Last_name` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `Gender` varchar(5) COLLATE latin1_general_cs NOT NULL,
+  `email` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `contact` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `Member_ID` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `Trainer_ID` varchar(12) COLLATE latin1_general_cs NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+--
+-- Dumping data for table `Members`
+--
+
+INSERT INTO `Members` (`First_name`, `Last_name`, `Gender`, `email`, `contact`, `Member_ID`, `Trainer_ID`) VALUES
+('Abhishek', 'Sr', 'Male', 'abii@abiistudio.com', '8951891675', '10asdas', '191');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Package`
 --
 
@@ -62,13 +85,27 @@ INSERT INTO `Package` (`Package_ID`, `Package_name`, `Amount`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Payment`
+--
+
+CREATE TABLE `Payment` (
+  `Payment_ID` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `Package_ID` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `Member_ID` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `Payment_type` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `Amount` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Trainer`
 --
 
 CREATE TABLE `Trainer` (
   `Trainer_ID` varchar(5) COLLATE latin1_general_cs NOT NULL,
   `Name` varchar(20) COLLATE latin1_general_cs DEFAULT NULL,
-  `Phone` int DEFAULT NULL
+  `Phone` varchar(20) COLLATE latin1_general_cs DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 --
@@ -76,11 +113,23 @@ CREATE TABLE `Trainer` (
 --
 
 INSERT INTO `Trainer` (`Trainer_ID`, `Name`, `Phone`) VALUES
-('191', 'Akshay', 222);
+('191', 'Akshay', '222'),
+('rr', 'rrr', '555555555'),
+('w', 'w', '33'),
+('wad', 'wad', '1111111111'),
+('ww', 'ww', '22222'),
+('wwww', 'wwww', '3333333');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Members`
+--
+ALTER TABLE `Members`
+  ADD PRIMARY KEY (`Member_ID`),
+  ADD KEY `Trainer_ID` (`Trainer_ID`);
 
 --
 -- Indexes for table `Package`
@@ -89,10 +138,34 @@ ALTER TABLE `Package`
   ADD PRIMARY KEY (`Package_ID`);
 
 --
+-- Indexes for table `Payment`
+--
+ALTER TABLE `Payment`
+  ADD PRIMARY KEY (`Payment_ID`),
+  ADD KEY `FK` (`Member_ID`,`Package_ID`);
+
+--
 -- Indexes for table `Trainer`
 --
 ALTER TABLE `Trainer`
   ADD PRIMARY KEY (`Trainer_ID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Members`
+--
+ALTER TABLE `Members`
+  ADD CONSTRAINT `members_ibfk_1` FOREIGN KEY (`Trainer_ID`) REFERENCES `Trainer` (`Trainer_ID`);
+
+--
+-- Constraints for table `Payment`
+--
+ALTER TABLE `Payment`
+  ADD CONSTRAINT `MID` FOREIGN KEY (`Member_ID`) REFERENCES `Package` (`Package_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`Member_ID`) REFERENCES `Members` (`Member_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- adminPanel.php
+++ adminPanel.php
@@ -1,5 +1,17 @@
 <?php
+    require("connection.php");
+    require("../credentials.php");
     session_start();
+    $uid = rand(0001,5000);
+    $user_list = mysqli_query($connectionObj,"SELECT * FROM Members");
+    while ($user = mysqli_fetch_array($user_list))
+    {
+        if ($uid == $user['Member_ID'])
+        {
+            echo '<script type="text/javascript">location.reload(true);</script>';
+        }
+    }
+
     if(!isset($_SESSION['admin_username']))
     {
         header("location: admin_login.php");
@@ -9,29 +21,253 @@
         session_destroy();
         header("location: admin_login.php");
     }
+    // if(isset($_POST['delete-btn']))
+    // {
+    //     $delName=$_POST['delUser'];
+    //     if($delName==$_SESSION['admin_username'])
+    //     {
+    //         echo "<script>alert('User Active! Deletion failed!')</script>";
+    //     }
+    //     else
+    //     {
+    //         $delQuery="DELETE FROM `admin_login` WHERE `admin_name`='$delName';";
+    //         $delResult=mysqli_query($connectionObj,$delQuery);
+    //         if($delResult)
+    //         {
+    //             echo "<script>alert('Deleted!')</script>";
+    //             echo '<script type="text/javascript">location.reload(true);</script>';
+                
+    //         }
+            
+    //     }
+
+    // }
+    
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <link rel="stylesheet" href="adminPanelStyle.css">
-    <title>Admin Panel</title>
-</head>
-<body>
+<html lang="en" >
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+        <link rel="stylesheet" href="adminPanelStyle.css">
+        <title>Register</title>
+    </head>
+    <body ><?php include("./HeaderSidebar.php");?>
+        <div class="content">
+        </div>
+        <style>
+            #check:checked ~ .container
+            {
+        
+                z-index: 2;
+                position: fixed;
+                left: 8%; 
+                top: 10%;
+                background-color: rgba(0, 0, 00, 0.3);
+                background-position: center;
+                background-size: cover;
+                float: center;
+                height: 88%;
+                width: 90%;
+                opacity: 100%;
+                border-radius: 20px;
+                position: center;
+                background-size: cover;  
+                transition: 0.5s;  
+                margin-left: 30px;
+                margin-right: 80px;
+                padding: 20px;
+                max-width: 80%;
+                color: white;
+                padding-right: 20px;
+                max-height: 80%;
+                overflow-y: scroll;
+                
+            }
+            .container
+            {
+                z-index: 2;
+                position: fixed;
+                left: 18%; 
+                top: 10%;
+                background-color: rgba(0, 0, 00, 0.3);
+                background-position: center;
+                background-size: cover;
+                float: center;
+                height: 88%;
+                width: 90%;
+                opacity: 100%;
+                border-radius: 20px;
+                position: center;
+                background-size: cover;  
+                transition: 0.5s;  
+                margin-left: 150px;
+                padding:20px;
+                max-width: 58%;
+                color: white;
+                padding-right: 20px;
+                max-height: 80%;
+                overflow-y: scroll;
+            }
+            .activityButton
+            {
+                padding: 5px;
+                background: #ffffff;
+                text-decoration: none;
+                
+                /* margin-top: -30px;
+                margin-right: 40px; */
+                border-radius: 5px;
+                font-size: 15px;
+                font-weight: 600;
+                color: rgb(224, 125, 67);
+                transition: 0.5s;
+                margin-left: 20px;
+                transition-property: background;
+            }
     
-    <?php include("./HeaderSidebar.php");
-    
-    ?>
-    <div class="content">
-    </div>
+            .activityButton:hover
+            {
+                background: #f78f5f;
+                color:rgb(255, 255, 255)
+            }
+            
+            
+        </style>
+        <div class="container">
 
-    
 
 
-    
+            <!-- registration section start -->
+            <h1><center>Member Registration</center></h1>
+            
+            <form action="adminPanel.php" method="post" > 
+                <table>
+                <div><tr>
+                                
+                                <td>Member ID</td>
+                                <!-- <td><input type="text" name="setMemberID"> </td> -->
+                                <td><?php echo "$uid";?></td>
+                            </tr>    
+                        </div>
+                        <div>
+                            <tr>
+                                
+                                <td>First Name</td>
+                                <td><input type="text" name="setFN"></td>
+                            </tr>    
+                        </div>   
 
-</body>
+                        <div><tr>
+                                
+                                <td>Last Name</td>
+                                <td><input type="text" name="setLN"> </td>
+                            </tr>    
+                        </div>
+                        <div>
+                            <tr>
+                                
+                                <td>Gender</td>
+                                <td>
+                                    <input type="radio" id="male" name="setGender" value="male">
+                                    <label for="male">Male</label><br>
+                                    <input type="radio" id="female" name="gender" value="female">
+                                    <label for="female">Female</label><br>
+                                    <input type="radio" id="other" name="gender" value="other">
+                                    <label for="other">Other</label></td>
+                            </tr>  
+                        </div> 
+                        <div><tr>
+                                
+                                <td>e-mail</td>
+                                <td><input type="text" name="setEmail"> </td>
+                            </tr>    
+                        </div> 
+                        <div><tr>
+                                
+                                <td>Contact</td>
+                                <td><input type="text" name="setPhone"> </td>
+                            </tr>    
+                        </div>
+                        
+                        <div><tr>
+                                
+                                <td>Trainer</td>
+                                <td><select name="setTrainerID">
+                                <?php 
+                                    $query="SELECT * FROM `Trainer`;";
+                                    $result=mysqli_query($connectionObj,$query);
+                                    while($row = mysqli_fetch_array($result))
+                                        {
+                                            $trainerName=$row['Name'];
+                                            $trainerID=$row['Trainer_ID'];
+                                            echo "<option value=$trainerID>$trainerID - $trainerName</option>";
+                                        }
+                                ?>
+
+                </select> </td>
+                            </tr>    
+                        </div>
+
+                        <tr>
+                            <td></td>
+                            <td><button type="submit" name="create_btn" class="activityButton">Create</button></td>
+                        </tr> 
+                </table>
+            </form>
+                
+            
+            <?php
+                global $connectionObj;
+                if(isset($_POST['create_btn']))
+                {
+                
+                    $setFN= mysqli_real_escape_string($connectionObj,$_POST['setFN']);
+                    $setLN= mysqli_real_escape_string($connectionObj,$_POST['setLN']);
+                    $setGender= mysqli_real_escape_string($connectionObj,$_POST['setGender']);
+                    $setEmail= mysqli_real_escape_string($connectionObj,$_POST['setEmail']);
+                    $setPhone= mysqli_real_escape_string($connectionObj,$_POST['setPhone']);
+                    $setTrainerID= mysqli_real_escape_string($connectionObj,$_POST['setTrainerID']);
+                    if(empty($setFN))
+                        {echo "<script>alert('Empty First Name');</script>";}
+                    else if(empty($setLN))
+                        {echo "<script>alert('Empty Last Name!');</script>";}
+                    else if(empty($setGender))
+                        {echo "<script>alert('Empty Gender!');</script>";}
+                    else if(empty($setEmail))
+                        {echo "<script>alert('Empty e-mail!');</script>";}
+                    else if(empty($setPhone))
+                        {echo "<script>alert('Empty Phone Number!');</script>";}
+                    else if(!(is_numeric($setPhone)) )
+                        {echo "<script>alert('Invalid Phone Number error!');</script>";}
+                    else 
+                    {   
+                            $query="INSERT INTO `Members` (`First_name`, `Last_name`, `Gender`, `email`, `contact`, `Member_ID`, `Trainer_ID`) VALUES
+                            ('$setFN', '$setLN', '$setGender', '$setEmail', '$setPhone', '$uid', '$setTrainerID');";
+                            $res=mysqli_query($connectionObj,$query);
+                            if($res)
+                            {
+                                echo "<script>alert('Member Created!');</script>";
+                            }
+                            
+                            echo '<script type="text/javascript">location.reload(true);</script>';
+                        
+                        
+                    }
+                }
+
+
+
+            ?>
+            
+
+
+            <!-- registration section end -->
+        </div>
+
+            
+            
+    </body>
 </html>
